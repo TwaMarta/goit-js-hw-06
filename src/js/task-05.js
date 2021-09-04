5ba2fd4cc7b938018bedada9dce23e392d811510
TwaMarta <twamarta@gmail.com>
2021-09-04 14:57:13 +0200

    1

--- src/js/task-05.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
-
-const name = () => {
-  output.textContent = input.value;
-};
-
-input.addEventListener("input", name);
--- src/js/task-09.js
+++
@@ -1,9 +0,0 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-function colorChanger() {
-  const colorName = document.querySelector(".color");
-  colorName.textContent = (getRandomHexColor(""));
-  document.body.style.backgroundColor=(getRandomHexColor(""));
-}
-const btn = document.querySelector(".change-color").addEventListener("click", colorChanger);
